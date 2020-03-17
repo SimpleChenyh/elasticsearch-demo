0e06715c07a85c6650da78f57930126a32967bde
chenyh <chenyh.me@gmail.com>
2020-03-17 21:38:06 +0800

    Add Elastic Search Util

---
+++ src/main/java/com/chenyh/util/EsUtil.java
@@ -0,0 +1,15 @@
+package com.chenyh.util;
+
+import org.elasticsearch.client.ElasticsearchClient;
+
+/**
+ * @program:
+ * @description:
+ * @author: HenryChen
+ * @create: 20-3-16 下午10:38
+ **/
+public class EsUtil {
+
+
+
+}
