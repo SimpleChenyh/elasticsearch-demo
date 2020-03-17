4695ae10444aad25cc960e83aaeeade9e9d3bc48
chenyh <chenyh.me@gmail.com>
2020-03-17 21:37:36 +0800

    Add Elastic Search Configuration

---
+++ src/main/java/com/chenyh/config/ElasticSearchConfiguration.java
@@ -0,0 +1,28 @@
+package com.chenyh.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+/**
+ * @program:
+ * @description:
+ * @author: HenryChen
+ * @create: 20-3-17 上午7:25
+ **/
+@Configuration
+@Component
+public class ElasticSearchConfiguration {
+
+//    @Bean
+//    RestClientAutoConfiguration.RestHighLevelClientConfiguration client() {
+//
+//        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+//                .connectedTo("localhost:9200", "localhost:9201")
+//                .build();
+//
+//        return RestClients.create(clientConfiguration).rest();
+//    }
+
+
+
+}
